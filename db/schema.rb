1432074695283dd3d65eb5e110cdb2df11356970
Gerald Ang <geraldangw@gmail.com>
2016-07-22 11:01:24 +0800

    heroku configurations 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721180719) do
+ActiveRecord::Schema.define(version: 20160721173321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
